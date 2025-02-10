6d3e5cb27551e58192b96c83b2ef1fd8506889b4
sefadas <dasefa@hotmail.com>
2025-02-11 01:07:04 +0300

    feat: Footer

---
+++ shopping/app/(routes)/_components/Footer.tsx
@@ -0,0 +1,12 @@
+import { Label } from "@/components/ui/label";
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="text-center border-t py-4 borderone">
+      <Label>Copyright © Sefa Das</Label>
+    </footer>
+  );
+};
+
+export default Footer;
